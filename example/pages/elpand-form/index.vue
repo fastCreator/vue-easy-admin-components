39a032b02d02c2ebb40801f2763015fcf21c6ade
zhangguo <zhangguo@inke.cn>
2019-08-29 20:43:22 +0800

    form

---
+++ example/pages/elpand-form/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="elpand-form-demo">
+    <elpand-form
+      v-bind="bind"
+      v-if="visible"
+      @close="close"
+    ></elpand-form>
+  </div>
+</template>
+<script>
+import { Promise } from 'q';
+export default {
+  data() {
+    return {
+      visible: true,
+      bind: {
+        dialog: {
+          bind: {
+            title: '新增',
+            width: '500px'
+          },
+          on: {}
+        },
+        form: {
+          data: {
+            name: '张珊珊'
+          },
+          on: {},
+          bind: {
+            labelWidth: '80px'
+          },
+          list: [
+            {
+              tag: 'el-input',
+              prop: 'name',
+              label: '姓名',
+              itemBind: {
+                rules: [{ required: true, message: '姓名不能为空' }]
+              }
+            }
+          ],
+          submit(o) {
+            console.log(o)
+            return new Promise(r => {
+              setTimeout(() => {
+                r()
+              }, 1000);
+            })
+          }
+        }
+      }
+    }
+  },
+  created() {
+  },
+  methods: {
+    close() {
+      this.visible = false
+    }
+  }
+}
+</script>
+<style lang="less">
+.elpand-form-demo {
+}
+</style>
---
+++ lib/elpand-form/src/form.vue
@@ -0,0 +1,76 @@
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true
+    },
+    on: {
+      type: Object,
+      default: () => ([])
+    },
+    bind: {
+      type: Object,
+      default: () => ([])
+    },
+    list: {
+      type: Array,
+      required: true
+    },
+    submit: {
+      type: Function,
+      required: true
+    }
+  },
+  methods: {
+    validate() {
+      return this.$refs.form.validate().then(async () => {
+        return this.submit(this.data)
+      })
+    },
+    getFormItem(h) {
+      return this.list.map(it => {
+        return h('el-form-item', {
+          props: {
+            ...it.itemBind,
+            label: it.label,
+            prop: it.prop,
+          },
+          on: {
+            ...it.itemOn
+          }
+        }, [
+            h(it.tag, {
+              props: {
+                ...it.bind,
+                value: this.data[it.prop]
+              },
+              on: {
+                ...it.on,
+                input: (v) => {
+                  this.data[it.prop] = v
+                }
+              }
+            })
+          ])
+      })
+    }
+  },
+  render(h) {
+    h = this.createElement || h
+    const { bind, on, data } = this
+    const { getFormItem } = this
+    return h('el-form', {
+      props: {
+        ...bind,
+        model: data
+      },
+      on: {
+        ...on
+      },
+      class: 'elpand-form',
+      ref: 'form'
+    }, getFormItem(h))
+  }
+}
+</script>
