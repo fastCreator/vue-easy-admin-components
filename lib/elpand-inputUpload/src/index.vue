928eb514faf2e9daec254edac05df4a181f4de32
zhangguo <zhangguo@inke.cn>
2019-08-30 19:07:08 +0800

    input-upload

---
+++ lib/elpand-inputUpload/src/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <el-row class="elpand-inputUpload">
+    <el-col
+      :span="18"
+      class="input-wrap"
+    >
+      <el-input
+        :value="value"
+        @input="input"
+        :placeholder="placeholder"
+      >
+      </el-input>
+    </el-col>
+    <el-col :span="4">
+      <el-upload
+        :multiple="false"
+        :auto-upload="false"
+        :fileList="[]"
+        :show-file-list="false"
+        :accept="accept"
+        :action="$attrs.action||''"
+        :on-change="change"
+        v-bind="$attrs"
+        v-on="_events"
+      >
+        <el-button type="primary">上传</el-button>
+      </el-upload>
+    </el-col>
+  </el-row>
+</template>
+<script>
+import emitter from '../../utils/emitter';
+import { MIME_type } from '../../utils/commom'
+export default {
+  name: 'elpand-inputUpload',
+  mixins: [emitter],
+  props: {
+    upload: {
+      type: Function,
+      require: true,
+    },
+    value: {
+      type: String,
+      require: true,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: '请输入'
+    },
+    type: {
+      type: Array,
+      default: () => (['png', 'jpg', 'jpeg'])
+    }
+  },
+  computed: {
+    accept() {
+      return this.type.map(it => MIME_type[it] || it).join(',')
+    }
+  },
+  methods: {
+    input(v) {
+      this.$emit('input', v)
+    },
+    async change(e) {
+      this.input(await this.upload(e))
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.elpand-inputUpload {
+  .input-wrap {
+    padding-right: 14px;
+  }
+}
+</style>
