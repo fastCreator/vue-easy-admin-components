f229347f74860610a9863ca826130a0a7393593c
zhangguo <zhangguo@inke.cn>
2019-09-25 15:15:31 +0800

    elpand-pageA

---
+++ lib/elpand-pageA/src/index.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="elpand-pageA">
+ Hello elpand-pageA
+</div>
+</template>
+<script>
+export default {
+  name: 'elpand-pageA',
+  props: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  created () {
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="less">
+.elpand-pageA{
+
+}
+</style>
